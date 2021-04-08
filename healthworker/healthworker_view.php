0ff5ade0c4501f64195b5e889a2963269b83cd40
chimirk <chirca.mircea@gmail.com>
2021-04-08 15:40:27 -0400

    Update healthworker_view.php

--- healthworker/healthworker_view.php
+++ healthworker/healthworker_view.php
@@ -19,23 +19,51 @@ $screenData=getBulkData($QueryToRun);
         </div>
         <div>
             <form id="new_schedule" class="form-horizontal" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                <input type="hidden" name="person_id" value="<?php echo $q?>">
-                <!--<input type="hidden" name="facility_id" value="<?php /*echo $screenData['facility_id']*/?>">-->
+                <label for="person_id"> <!--person id--></label>
+                <input type="hidden" name="person_id" id="person_id" value="<?php echo $q?>">
+                <label for="facility_id"> <!--facility id--></label>
+                <input type="hidden" name="facility_id" id="facility_id" value="<?php echo $screenData['facility_id'];?>"/>
                 <div class="form-group">
                     <label for="schedule_date">
                         <strong>Choose Schedule Date</strong>
                     </label>
-                    <input type="text" name="schedule_date" class="form-control" id="schedule_date" placeholder="dd-mm-yyyy">
+                    <input type="date" name="schedule_date" class="form-control" id="schedule_date" placeholder="dd-mm-yyyy">
+                </div>
+
+                <div class="form-group">
+                    <label for="schedule_start_time">
+                        Schedule Start Time
+                    </label>
+                    <input type="time" class="form-control" id="schedule_start_time" name ="schedule_start_time"
+                           required/>
+                </div>
+                <div class="form-group">
+                    <label for="schedule_end_time">
+                        Schedule End Time
+                    </label>
+                    <input type="time" class="form-control" id="schedule_end_time" name ="schedule_end_time" required/>
                 </div>
                 <div class="form-group">
                     <label for="facility_name">
+                        Facility Name
+                    </label>
+                    <input type="text" class="form-control" id="facility_name" name ="facility_name"  value=
+                    "<?php
+                    $facility_name= $screenData['facility_name'];
+                    echo $facility_name;
+                    ?>
+                        " required readonly/>
+                </div>
+                <!--<div class="form-group">
+                    <label for="facility_name">
                         Choose Public Health Center
                     </label>
+                    <input type="text" class="form-control" id="facility_name">
                     <select name="facility_name" id="facility_name" >
-                        <?php echo @getPublicHealthCenters();
-                        ?>
+                        <?php /*echo @getPublicHealthCenters();
+                        */?>
                     </select>
-                </div>
+                </div>-->
             </form>
 
             <button form="new_schedule" type="submit" class="btn btn-primary" name="create_new_schedule">Add New Schedule</button>
@@ -53,19 +81,17 @@ $screenData=getBulkData($QueryToRun);
                 }
                 ?></span>
 
-
             </h4>
             <table class="table">
                 <?php
-                //echo $q;
-                //$QueryToRun="SELECT * FROM `healthworker_det_schedule_view` WHERE `person_id`='$q'";
-                //$screenData=getBulkData($QueryToRun);
-                //echo $screenData['person_id'];
                 $table_name = 'healthworker_det_schedule_view';
                 displaySchedules($table_name, $q);
                 ?>
             </table>
         </div>
+
+
+
 
 
 </body>
